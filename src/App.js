dbee10973da11710a05ee6fcae00ed432ba9fe9b
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2023-09-11 13:43:46 +0200

    Update capitalization

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 export default function App() {
   const initialEmoji = emoji.random();
   const [emojiIcon, setEmojiIcon] = useState(initialEmoji.emoji);
-  // 1. create the state for the input
+  // 1. Create the state for the input
   const [emojiName, setEmojiName] = useState(initialEmoji.name);
   // This return true if the current value of emojiName is invalid
   const hasError = !emoji.has(emojiName);
@@ -17,24 +17,25 @@ export default function App() {
           fontSize: '100px',
         }}
       >
+        {/* Show the emoji on the screen */}
         {emojiIcon}
       </div>
       <div style={{ color: 'white', backgroundColor: 'red' }}>
         {hasError ? "This emoji don't exist" : ''}
       </div>
       <br />
-      {/* 2. set the value of the input*/}
-      {/* 3. use the update function inside of the onChange event */}
+      {/* 2. Set the value of the input */}
+      {/* 3. Use the update function inside of the onChange event */}
       <input
         value={emojiName}
         onChange={(event) => {
           setEmojiName(event.currentTarget.value);
-          // 1. it matches update the emoji with the new value
+          // 1. It matches update the emoji with the new value
           if (emoji.has(event.currentTarget.value)) {
             const foundEmojiIcon = emoji.get(event.currentTarget.value);
             setEmojiIcon(foundEmojiIcon);
           } else {
-            // 2. the name doesn't matches show error
+            // 2. The name doesn't matches show error
             setEmojiIcon('');
           }
         }}
